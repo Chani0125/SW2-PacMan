9e4f65a21ff8863a3f763bcf11cb296ee17521f5
Chani0125 <ap25ap25@naver.com>
2022-12-06 20:03:28 +0900

    Make Display Map

--- enemy.py
+++ enemy.py
@@ -1,2 +1,26 @@
 class Ghost:
-    pass
+
+    def __init__(self, direct, shape):
+        self.direct = direct
+        self.shape = shape
+        self.shapeChange = True
+
+    def changeShape(self):
+        if self.shapeChange:
+            self.shape = 1 - self.shape
+
+    def setDirect(self, direct):
+        self.direct = direct
+
+    def getShape(self):
+        return self.shape
+
+    def getDirect(self):
+        if self.direct == 0:
+            return "Right"
+        elif self.direct == 1:
+            return "Left"
+        elif self.direct == 2:
+            return "Down"
+        else:
+            return "Up"
--- player.py
+++ player.py
@@ -1,2 +1,25 @@
 class PacMan:
-    pass
+
+    def __init__(self):
+        self.shape = 1
+        self.increase = False
+        self.shapeChange = True
+
+    def changeShape(self, idx=-1):
+        if idx < 0:
+            if self.shapeChange:
+                if self.increase:
+                    self.shape += 1
+                    if self.shape == 2:
+                        self.increase = False
+                else:
+                    self.shape -= 1
+                    if self.shape == 0:
+                        self.increase = True
+        elif idx < 5:
+            self.shape = idx
+        else:
+            print("Error: PacMac Shape Index Error")
+
+    def getShape(self):
+        return self.shape
